b97fb2500c8b054ccc76a4c83198c6b72358c551
PrachetShah <prachetshah25@gmail.com>
2021-09-13 13:17:35 +0530

    state components

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,20 @@
+import { useState } from "react";
+
+
 const Home = () => {
+    // let name="Prachet";
+    const [name, setName] = useState('Prachet')
+
     const handleClick = () => {
-        console.log("Hola Ninjas!!")
+        setName('Kevin')
     }
     
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name}</p>
             <button onClick={ handleClick }>Click Me</button>
+            {/* Create Anonymous Func so we can pass in values and fucntion is not called whenevr site is loaded */}
         </div>
     );
 }
