bc013230c5d97960d68b79fed6312a9c97412df3
PrachetShah <prachetshah25@gmail.com>
2021-09-13 18:05:10 +0530

    Adding props and make delete button working using useState

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,24 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'My New Website', body: "Lorem ipsum, dolor sit amet consectetur adipisicing elit", author: "Prachet", id:1 },
         { title: 'My New LEGO Model', body: "Lorem ipsum, dolor sit amet consectetur adipisicing elit", author: "Kevin", id:2 },
-        { title: 'My New Cooking Video', body: "Lorem ipsum, dolor sit amet consectetur adipisicing elit", author: "Bhoomi", id:3 },
+        { title: 'My New Cooking Video', body: "Lorem ipsum, dolor sit amet consectetur adipisicing elit", author: "Prachet", id:3 },
     ]);
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    };
+
     return ( 
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>{ blog.author }</p>
-                </div>
-            ))}
+            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+            {/* To segregate Blogs
+            <BlogList blogs={ blogs.filter((blog)=> blog.author === 'Prachet') } title="Prachet's Blogs" /> */}
             {/* Create Anonymous Func so we can pass in values and fucntion is not called whenevr site is loaded */}
         </div>
     );
