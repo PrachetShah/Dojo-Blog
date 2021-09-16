e1afcfa2c5b4a45c9953a354b5fe076e9f09e396
PrachetShah <prachetshah25@gmail.com>
2021-09-16 09:16:14 +0530

    handlind and catching errors on fetch request

--- src/Home.js
+++ src/Home.js
@@ -1,37 +1,51 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
-
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const[error, setError] = useState(null);
 
-    useEffect(() => {
-        fetch("http://localhost:8000/blogs")
-            .then(res => {
-                    return res.json();
-            })
-            .then((data) => {
-                console.log(data);
-                setBlogs(data)
-            })
-    }, []);
+  // npx json-server --watch data/db.json --port 8000 to run the fetch request database
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+        .then(res => {
+            console.log(res)
+            if(!res.ok){
+                throw Error('Could not fetch the data for that resource');
+            }
+            return res.json();
+        })
+        .then(data => {
+            setBlogs(data);
+            setIsPending(false);
+            setError(null);
+        })
+        .catch(err => {
+            setError(err.message);
+            setIsPending(false);
+        })
+    }, 1000);
+  }, [])
 
     // //Now it will run again only when name changes.
     // useEffect(() => {
     //     console.log('Use Effect Executed');
     //     console.log(name);
     // }, [name]);
-        //Now it will run again only when name changes.
-    
+    //Now it will run again only when name changes.
 
-    return ( 
-        <div className="home">
-            {blogs && <BlogList blogs={blogs} title="All Blogs" />}
-            {/* To segregate Blogs
+  return (
+    <div className="home">
+        {error && <div>{error}</div>}
+      { isPending && <div>Loading...</div> }
+      {blogs && <BlogList blogs={blogs} />}
+       {/* To segregate Blogs
             <BlogList blogs={ blogs.filter((blog)=> blog.author === 'Prachet') } title="Prachet's Blogs" /> */}
-            {/* Create Anonymous Func so we can pass in values and fucntion is not called whenevr site is loaded */}
-        </div>
-    );
+        {/* Create Anonymous Func so we can pass in values and fucntion is not called whenevr site is loaded */}
+    </div>
+  );
 }
  
 export default Home;
